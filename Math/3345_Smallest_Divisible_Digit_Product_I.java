fc77e1dd2d87bfd10e49939fd7a8a29d5eec1419
Supritha Sekar <suprithasekar07@gmail.com>
2026-08-06 21:42:40 +0530

    Add LeetCode #3345 - Smallest Divisible Digit Product I

---
+++ Math/3345_Smallest_Divisible_Digit_Product_I.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while (true) {
+            if (digitProduct(n) % t == 0) {
+                return n;
+            }
+            n++;
+        }
+    }
+    public int digitProduct(int num) {
+        int product = 1;
+
+        while (num > 0) {
+            product *= (num % 10);
+            num /= 10;
+        }
+         return product;
+    }
+}
